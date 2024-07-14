5cdb3fefd48691ebfb52663e2851cceaa413700e
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2024-07-13 21:03:04 -0700

    Connected to DB

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/workouts', workoutRoutes)
 
-app.listen(process.env.PORT, () => {
-  console.log('Listening on port 2121')
-})
+//connect to DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() =>{
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to DB and listening on port ${process.env.PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
