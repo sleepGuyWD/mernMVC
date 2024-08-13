3fdc79c19d5fdbf10be6bcee733cff0a583a83dd
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2024-08-12 21:12:11 -0700

    Started Delete portion

--- frontend/src/components/card.jsx
+++ frontend/src/components/card.jsx
@@ -1,12 +1,26 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
 function Card({workout}) {
+
+  const handleClick = async () => {
+    const res = await fetch ('/api/workouts/'+ workout._id, {
+      method: 'DELETE'
+    })
+
+    const json = await res.json()
+
+    if (res.ok) {
+
+    }
+
+  }
   return(
     <div className="workout-details">
-      
       <h4>{workout.title}</h4>
       <p>Load (lbs): {workout.load}</p>
       <p>Reps: {workout.reps}</p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>Delete</span>
     </div>
 
   )
